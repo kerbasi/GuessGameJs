089518855179f11a54650cd85ef99bea28307511
Kirill Novikov <kirill.novikov@gmail.com>
2022-10-03 09:04:59 +0300

    Add first strucher to html, add js

---
+++ index.js
@@ -0,0 +1,19 @@
+﻿let userAttemptsCount = 10;
+let minRange = 0;
+let maxRange = 10;
+let userNumber;
+
+const minRangeEl = document.getElementById("minRange");
+const maxRangeEl = document.getElementById("maxRange");
+const userNumberEl = document.getElementById("userNumber");
+const attemptsEl = document.querySelector("#user_attempts");
+
+minRangeEl.setAttribute("value", minRange);
+maxRangeEl.setAttribute("value", maxRange);
+attemptsEl.textContent += userAttemptsCount;
+
+if (userNumberEl.value) {
+    console.log("Got it")
+} else {
+    console.log("There is no number")
+}
