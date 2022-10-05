4fc054b2a00432fc3965e4e096529634ffba1c26
Kirill Novikov <kirill.novikov@gmail.com>
2022-10-05 09:09:02 +0300

    Add new buttons

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-﻿let userAttemptsCount = 10;
-let minRange = 0;
-let maxRange = 10;
-let userNumber;
+﻿let userAttemptsCount = 0
+let minRange = 0
+let maxRange = 10
+let userNumber
+let guessedNumber
+const minRangeEl = document.getElementById("minRange")
+const maxRangeEl = document.getElementById("maxRange")
+const userNumberEl = document.getElementById("userNumber")
+const attemptsEl = document.querySelector("#user_attempts")
+const tryButtonEl = document.querySelector("#try_button")
+const newGameEl = 
+userNumber = userNumberEl.value
 
-const minRangeEl = document.getElementById("minRange");
-const maxRangeEl = document.getElementById("maxRange");
-const userNumberEl = document.getElementById("userNumber");
-const attemptsEl = document.querySelector("#user_attempts");
+minRangeEl.setAttribute("value", minRange)
+maxRangeEl.setAttribute("value", maxRange)
+attemptsEl.textContent += userAttemptsCount
+tryButtonEl.addEventListener('click', tryButtonClicked)
 
-minRangeEl.setAttribute("value", minRange);
-maxRangeEl.setAttribute("value", maxRange);
-attemptsEl.textContent += userAttemptsCount;
+function tryButtonClicked() {
+    
+    if(userNumberEl.value) {
+        userNumber = userNumberEl.value
+        if(userNumber == guessedNumber){
+            console.log("You WON")
+        } else if(userNumber > guessedNumber){
+            console.log("It's too big")
+        } else {
+            console.log("It's too small")
+        }
+    } else {
+        console.log("Please pick a number")
+    }
+}
 
-if (userNumberEl.value) {
-    console.log("Got it")
-} else {
-    console.log("There is no number")
+function generateNumber(min, max){
+    return Math.round((Math.random() * (max - min)) + min)
+}
+
+function newGame(){
+    guessedNumber = generateNumber(maxRange, maxRange)
+    userAttemptsCount = 10
 }
